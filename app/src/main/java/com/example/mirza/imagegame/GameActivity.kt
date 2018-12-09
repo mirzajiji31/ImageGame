1ce8c6d2ba434e91bb4eaec13499d7c07871b6d6
mirzajiji31 <45181319+mirzajiji31@users.noreply.github.com>
2018-12-10 02:10:19 +0400

    d

--- app/src/main/java/com/example/mirza/imagegame/GameActivity.kt
+++ app/src/main/java/com/example/mirza/imagegame/GameActivity.kt
@@ -128,7 +128,6 @@ class GameActivity : AppCompatActivity() {
                         PorterDuff.Mode.SRC_ATOP
                     )
             }
-
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
